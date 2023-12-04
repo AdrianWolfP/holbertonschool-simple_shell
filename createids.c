eeb3ecfd5f9d06c543828df971e70b12f8c4312a
AdrianWolfP <wolfy52032@gmail.com>
2023-12-04 10:10:01 -0800

    fixing some betty isuues in createid and builtin

--- createids.c
+++ createids.c
@@ -5,22 +5,22 @@
 #include "headers.h"
 /**
  * main - creates child processes and prints the PID for each
- *
+ * printpids - Gets the awenser from child and prints it out to the parent
  * Return: Always 0
  */
 
-void printpids()
+void printpids(void)
 {
 	int i;
-	
-	for(i = 0; i < 5 ; i++)
+
+	for (i = 0; i < 5 ; i++)
 	{
-		if(fork() == 0)
+		if (fork() == 0)
 		{
 			printf("[Child] pid %d from [PaPa] pid %d\n", getpid(), getppid());
 			exit(0);
 		}
 	}
-	for(i = 0; i < 5 ; i++)
+	for (i = 0; i < 5 ; i++)
 		wait(NULL);
 }
