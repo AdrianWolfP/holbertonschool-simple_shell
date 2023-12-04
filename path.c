8825b8b6cca6caca0a447ebf9b1769a8b9fc4724
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-03 17:57:51 -0800

    comment edit

--- path.c
+++ path.c
@@ -1,16 +1,17 @@
-//#include <stdlib.h>
-//#include "headers.h"
-/**
+/*
+ * #include <stdlib.h>
+ *#include "headers.h"
+ *
  * get_path - function searching enviorment list to find the 
  * env variable name and returns a pointer to value string
  *
  * Return: Pointer to the corresponding value string
+ *
+ *char get_path(char *command)
+ *{
+ *	char *path;
+ *	char *path_copy = strdup(path);
+ *
+ *	path = getenv("PATH");
+ *}
  */
-//char get_path(char *command)
-//{
-//	char *path;
-//	char *path_copy = strdup(path);
-//
-//	path = getenv("PATH");
-//}
-//
